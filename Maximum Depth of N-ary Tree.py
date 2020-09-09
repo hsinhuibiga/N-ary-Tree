585cd37cd20d64bb64c9836e702df000f404f15c
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-09 23:26:57 +0800

    Add files via upload

---
+++ Maximum Depth of N-ary Tree.py
@@ -0,0 +1,22 @@
+#Maximum Depth of N-ary Tree
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.children:
+            return 1
+        depth = 1 + max(self.maxDepth(child) for child in root.children)
+        return depth
